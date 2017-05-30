bab7a5b854c16c78aad150b055dabb2d8450d4f1
sjf <1982764150@qq.com>
2017-05-30 21:28:55 +0800

    添加删除类别

--- WebContent/js/typeManage.js
+++ WebContent/js/typeManage.js
@@ -9,11 +9,32 @@ window.onload = function () {
         if (data.length > 0) {
             for (var i = 0; i < data.length; i++) {
                 var listItem = document.createElement('tr')
-                var html = '<td>' + data[i].name + '</td><td>' + data[i].count + '</td><td><a><i class="glyphicon glyphicon-remove"></i></a></td>'
+                var html = '<td>' + data[i].name + '</td><td>' + data[i].count + '</td><td><a class="delete">' +
+                    '<i name="' + data[i].name + '" class="glyphicon glyphicon-remove"></i></a></td>'
                 listItem.innerHTML = html
                 typeList.appendChild(listItem)
+                listItem.querySelector('.delete').onclick = function (event) {
+                    if (event.target.classList.contains('glyphicon-remove')) {
+                        console.log('123')
+                        var name = event.target.getAttribute('name')
+                        deleteType(name)
+                    }
+                }
             }
         }
+    }
+
+    function deleteCb(data) {
+        data = JSON.parse(data)
+        if (data.status === 'delete success') {
+            ajax("GET", "type", null, getList)
+        } else {
+            alert('delete fail')
+        }
+    }
+
+    function deleteType (name) {
+        ajax('DELETE', 'type', JSON.stringify({name: name}), deleteCb)
     }
 
     function init() {
